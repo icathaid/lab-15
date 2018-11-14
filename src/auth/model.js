e0c7782adc7e0d4876cca4efb656d0fd20a3c489
James McDaniel <icathaid@gmail.com>
2018-11-13 19:58:11 -0800

    added travis

--- src/auth/model.js
+++ src/auth/model.js
@@ -7,7 +7,7 @@ import jwt from 'jsonwebtoken';
 const userSchema = new mongoose.Schema({
   username: {type: String, required: true, unique: true},
   password: {type: String, required: true},
-  email: {type: String}
+  email: {type: String},
 });
 
 userSchema.pre('save', function(next) {
